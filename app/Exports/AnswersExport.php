6acfa34b510222bad9205df2b32b2c22603c4e1d
gatodelbass <davemmgarcia@gmail.com>
2026-01-27 18:37:37 -0500

    v1 completado

--- app/Exports/AnswersExport.php
+++ app/Exports/AnswersExport.php
@@ -4,13 +4,14 @@ namespace App\Exports;
 
 use App\Models\Play;
 use App\Models\UserAnswer;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
 use Maatwebsite\Excel\Concerns\Exportable;
 
-class AnswersExport implements FromQuery, WithHeadings, WithMapping
+class AnswersExport implements FromQuery, WithHeadings
 {
     use Exportable;
 
@@ -23,26 +24,44 @@ class AnswersExport implements FromQuery, WithHeadings, WithMapping
 
     public function query()
     {
-        return UserAnswer::where("play_id", $this->playId);
+
+       return DB::table('user_answers')
+        ->select(
+            'users.id',
+            'users.name',
+            'users.email',
+            'presentations.title as presentation',
+            'slides.title as slide',
+            'user_answers.question',
+            'user_answers.question_type',
+            'user_answers.answer',
+            'user_answers.correct',
+            'user_answers.play_id',
+            'user_answers.created_at'
+        )
+        ->join('users', 'users.id', '=', 'user_answers.user_id')
+        ->join('presentations', 'presentations.id', '=', 'user_answers.presentation_id')
+        ->join('slides', 'slides.id', '=', 'user_answers.slide_id')
+        ->where('user_answers.play_id', $this->playId)
+        ->orderBy('users.email');
     }
 
-     public function headings(): array
+    public function headings(): array
     {
         // Define the column headings for the Excel file
         return [
-            'ID',
-            'Name',
-            'Email Address'
+            'usu id',
+            'nombres',
+            'email',
+            'presentacion',
+            'slide',
+            'pregunta',
+            'tipo pregunta',
+            'respuesta',
+            'correcto',
+            'sesion id',
+            'fecha'
         ];
     }
 
-    public function map($user): array
-    {
-        // Map the selected database columns to the order of the headings
-        return [
-            $user->id,
-            $user->name,
-            $user->email
-        ];
-    }
 }
