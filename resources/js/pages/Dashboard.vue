9816568e558d74b14debfb64b80a37dea2f4c09b
gatodelbass <davemmgarcia@gmail.com>
2026-03-24 23:07:56 -0500

    botones al inicio

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -18,11 +18,18 @@ const breadcrumbs: BreadcrumbItem[] = [
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
 
-            dfg
+            
 
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                   sdfgsdf
+                    <div class="w-full p-2 text-xl">
+            <inertia-link :href="route('calculadoraIndice')" class="mx-1 rounded border-1 border-gray-400 bg-gray-200 px-1 py-0">
+                Indice masa corporal
+            </inertia-link>
+             <inertia-link :href="route('calculadoraApgar')" class="mx-1 rounded border-1 border-gray-400 bg-gray-200 px-1 py-0">
+                Apgar familiar
+            </inertia-link>
+        </div>
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
